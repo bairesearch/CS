4fa9d2ba75cf04212e17ea02ac8b4b188b3e2d85
Richard Baxter <richard.baxter@baxterai.com>
2017-01-15 13:32:46 +1100

    NO CHANGE RECORD

--- CS/CSoperations.h
+++ CS/CSoperations.h
@@ -26,7 +26,7 @@
  * File Name: CSoperations.h
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2014 Baxter AI (baxterai.com)
  * Project: Code Structure viewer
- * Project Version: 3d4a 8-June-2014
+ * Project Version: 3d4a 08-June-2014
  *
  *******************************************************************************/
 
