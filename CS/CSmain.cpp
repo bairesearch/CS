a326edae3433d17c66144f5dc5b474542ebe2069
Richard Baxter <richard.baxter@baxterai.com>
2017-01-15 13:30:34 +1100

    NO CHANGE RECORD

--- CS/CSmain.cpp
+++ CS/CSmain.cpp
@@ -3,7 +3,7 @@
  * File Name: CSmain.cpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2012 Baxter AI (baxterai.com)
  * Project: Code Structure viewer
- * Project Version: 3a8a 14-June-2012
+ * Project Version: 3a8b 14-June-2012
  *
  *******************************************************************************/
 
