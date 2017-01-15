5d8c86b9e9a3ae1bc3ace55a105b8fab0a3a9c4a
Richard Baxter <richard.baxter@baxterai.com>
2017-01-15 13:37:36 +1100

    NO CHANGE RECORD

--- CS/CSmain.cpp
+++ CS/CSmain.cpp
@@ -26,7 +26,7 @@
  * File Name: CSmain.cpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2015 Baxter AI (baxterai.com)
  * Project: Code Structure viewer
- * Project Version: 3f6a 27-July-2015
+ * Project Version: 3f7a 16-August-2015
  *
  *******************************************************************************/
 
@@ -282,7 +282,7 @@ int main(int argc,char* *argv)
 
 	if(argumentExists(argc,argv,"-version"))
 	{
-		cout << "OpenCS.exe - Project Version: 3f6a 27-July-2015" << endl;
+		cout << "OpenCS.exe - Project Version: 3f7a 16-August-2015" << endl;
 		exit(1);
 	}
 
