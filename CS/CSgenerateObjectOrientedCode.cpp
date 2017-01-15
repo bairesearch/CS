eb9e32d658c725e2909a4a462ef5f64a5e1da9b7
Richard Baxter <richard.baxter@baxterai.com>
2017-01-15 13:38:03 +1100

    NO CHANGE RECORD

--- CS/CSgenerateObjectOrientedCode.cpp
+++ CS/CSgenerateObjectOrientedCode.cpp
@@ -2,22 +2,17 @@
  *
  * This file is part of BAIPROJECT.
  *
- * BAIPROJECT is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License version 3
- * only, as published by the Free Software Foundation. The use of
- * intermediary programs or interfaces including file i/o is considered
- * remote network interaction. This does not imply such arrangements
- * do not constitute derivative works.
+ * BAIPROJECT is commercially licensed software. It may not be redistributed,
+ * and may only be used by a registered licensee.
  *
- * BAIPROJECT is distributed in the hope that it will be useful,
+ * BAIPROJECT is sold in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License version 3 for more details
+ * BAI Commercial License for more details
  * (a copy is included in the LICENSE file that accompanied this code).
  *
- * You should have received a copy of the GNU Affero General Public License
- * version 3 along with BAIPROJECT.  If not, see <http://www.gnu.org/licenses/>
- * for a copy of the AGPLv3 License.
+ * You will have received a copy of the BAI Commercial License
+ * along with BAIPROJECT. For more information, visit <http://www.baxterai.com>
  *
  *******************************************************************************/
 
@@ -26,7 +21,7 @@
  * File Name: CSgenerateObjectOrientedCode.cpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2015 Baxter AI (baxterai.com)
  * Project: Code Structure viewer
- * Project Version: 3f7d 16-August-2015
+ * Project Version: 3f7e 16-August-2015
  *
  *******************************************************************************/
 
