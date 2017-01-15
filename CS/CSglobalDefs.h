2f19b95f1d8d81e4d23dd40f6a4ba004df964a3a
Richard Baxter <richard.baxter@baxterai.com>
2017-01-15 13:37:47 +1100

    NO CHANGE RECORD

--- CS/CSglobalDefs.h
+++ CS/CSglobalDefs.h
@@ -26,7 +26,7 @@
  * File Name: CSglobalsDefs.h
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2015 Baxter AI (baxterai.com)
  * Project: Code Structure viewer
- * Project Version: 3f7b 16-August-2015
+ * Project Version: 3f7c 16-August-2015
  * Description: CS specific global definitions
  *
  *******************************************************************************/
