bd97930b90bc1d28a846ac35ba274dc00232855c
Richard Baxter <richard.baxter@baxterai.com>
2017-03-06 11:21:35 +1100

    CS 3j3d - update CSgenerateObjectOrientedCode and CSgenerateConstFunctionArgumentCode license headers

--- CS/CSgenerateHTMLdocumentation.cpp
+++ CS/CSgenerateHTMLdocumentation.cpp
@@ -26,7 +26,7 @@
  * File Name: CSgenerateHTMLdocumentation.cpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2017 Baxter AI (baxterai.com)
  * Project: Code Structure viewer
- * Project Version: 3j2b 17-January-2017
+ * Project Version: 3j3d 17-January-2017
  *
  *******************************************************************************/
 
@@ -66,7 +66,7 @@ string CSgenerateHTMLdocumentationClass::generateHTMLdocumentationHeader(const s
 	string HTMLdocumentationHeader = "";
 	if(htmlHeader)
 	{
-		HTMLdocumentationHeader = HTMLdocumentationHeader + "<html><head><title>" + name + " Documentation</title><style type=\"text/css\">TD { font-size:75%; } </style></head><body><h3>" + name + " Documentation</h3><p>Automatically generated with Code Structure Viewer (CS), Project Version: 3j2b 17-January-2017<p>\n";
+		HTMLdocumentationHeader = HTMLdocumentationHeader + "<html><head><title>" + name + " Documentation</title><style type=\"text/css\">TD { font-size:75%; } </style></head><body><h3>" + name + " Documentation</h3><p>Automatically generated with Code Structure Viewer (CS), Project Version: 3j3d 17-January-2017<p>\n";
 	}
 	else
 	{
--- CS/CSmain.cpp
+++ CS/CSmain.cpp
@@ -26,7 +26,7 @@
  * File Name: CSmain.cpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2017 Baxter AI (baxterai.com)
  * Project: Code Structure viewer
- * Project Version: 3j2b 17-January-2017
+ * Project Version: 3j3d 17-January-2017
  *
  *******************************************************************************/
 
@@ -307,7 +307,7 @@ int main(const int argc, const char** argv)
 
 	if(SHAREDvarsClass().argumentExists(argc, argv, "-version"))
 	{
-		cout << "CS.exe - Project Version: 3j2b 17-January-2017" << endl;
+		cout << "CS.exe - Project Version: 3j3d 17-January-2017" << endl;
 		exit(1);
 	}
 
