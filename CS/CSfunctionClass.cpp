b8e99325f5ca8b79e9bacda02c504705a21c412d
Richard Baxter <richard.baxter@baxterai.com>
2017-11-14 18:50:09 +1100

    NO CHANGE RECORD

--- CS/CSfunctionClass.cpp
+++ CS/CSfunctionClass.cpp
@@ -25,7 +25,7 @@
  * File Name: CSfunctionClass.cpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2017 Baxter AI (baxterai.com)
  * Project: Code Structure viewer
- * Project Version: 3m2a 10-July-2017
+ * Project Version: 3m3a 18-July-2017
  *
  *******************************************************************************/
 
