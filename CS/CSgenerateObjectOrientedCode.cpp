7ea7a6ba57e56b8ba7713bf93498713152bc1e76
Richard Baxter <richard.baxter@baxterai.com>
2019-04-26 02:08:27 +1000

    NO CHANGE RECORD

--- CS/CSgenerateObjectOrientedCode.cpp
+++ CS/CSgenerateObjectOrientedCode.cpp
@@ -26,7 +26,7 @@
  * File Name: CSgenerateObjectOrientedCode.cpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2019 Baxter AI (baxterai.com)
  * Project: Code Structure viewer
- * Project Version: 3m16a 24-April-2019
+ * Project Version: 3m16b 24-April-2019
  * /
  *******************************************************************************/
 
