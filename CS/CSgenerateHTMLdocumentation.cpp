c53de10526cb74b57f44ccf928ee0442116b81bf
Richard Baxter <richard.baxter@baxterai.com>
2021-03-18 09:20:42 +1100

    NO CHANGE RECORD

--- CS/CSgenerateHTMLdocumentation.cpp
+++ CS/CSgenerateHTMLdocumentation.cpp
@@ -26,7 +26,7 @@
  * File Name: CSgenerateHTMLdocumentation.cpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2021 Baxter AI (baxterai.com)
  * Project: Code Structure viewer
- * Project Version: 3p1a 15-March-2021
+ * Project Version: 3p2a 17-March-2021
  * /
  *******************************************************************************/
 
@@ -85,7 +85,7 @@ string CSgenerateHTMLdocumentationClass::generateHTMLdocumentationHeader(const s
 		
 	if(htmlHeader)
 	{
-		HTMLdocumentationHeader = HTMLdocumentationHeader + "<html><head><title>" + titleName + " Documentation</title><style type=\"text/css\">TD { font-size:75%; } </style></head><body><h3>" + titleName + " Documentation</h3><p>Automatically generated with Code Structure Viewer (CS), Project Version: 3p1a 15-March-2021</p>\n";
+		HTMLdocumentationHeader = HTMLdocumentationHeader + "<html><head><title>" + titleName + " Documentation</title><style type=\"text/css\">TD { font-size:75%; } </style></head><body><h3>" + titleName + " Documentation</h3><p>Automatically generated with Code Structure Viewer (CS), Project Version: 3p2a 17-March-2021</p>\n";
 		#ifdef CS_GENERATE_CLASS_HTML_DOCUMENTATION_ADD_COPYRIGHT
 		HTMLdocumentationHeader = HTMLdocumentationHeader + "<p>Copyright &copy; 2020, BAI Research. All Rights Reserved.</p>\n";
 		#endif
