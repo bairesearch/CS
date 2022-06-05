10e881b439abbb6ce026520b20d16833bc73d7b3
Richard Baxter <richard.baxter@baxterai.com>
2022-06-05 23:13:07 +1000

    NO CHANGE RECORD

--- CS/CSmain.cpp
+++ CS/CSmain.cpp
@@ -24,9 +24,9 @@
 /*******************************************************************************
  *
  * File Name: CSmain.cpp
- * Author: Richard Bruce Baxter - Copyright (c) 2005-2021 Baxter AI (baxterai.com)
+ * Author: Richard Bruce Baxter - Copyright (c) 2005-2022 Baxter AI (baxterai.com)
  * Project: Code Structure viewer
- * Project Version: 3p4c 07-August-2021
+ * Project Version: 3p5a 14-March-2022
  * /
  *******************************************************************************/
 
@@ -341,7 +341,7 @@ int main(const int argc, const char** argv)
 
 	if(SHAREDvarsClass().argumentExists(argc, argv, "-version"))
 	{
-		cout << "CS.exe - Project Version: 3p4c 07-August-2021" << endl;
+		cout << "CS.exe - Project Version: 3p5a 14-March-2022" << endl;
 		exit(EXIT_OK);
 	}
 
